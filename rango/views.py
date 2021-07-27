289319a2abee51ec83d070722235b9c903397d2c
Liang He <2538931h@student.gla.ac.uk>
2021-07-27 13:45:47 +0100

    ch4_v2:test OK

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,6 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page."+"<a href=\'/rango/\'>Index</a>")
-
+    # return HttpResponse("Rango says here is the about page."+"<a href=\'/rango/\'>Index</a>")
+    return render(request, 'rango/about.html')
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,11 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
@@ -129,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # Media files
