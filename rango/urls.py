b87f1e48d7345e4cfd08a532c959bf7b9033ffd8
Liang He <2538931h@student.gla.ac.uk>
2021-07-30 01:08:30 +0100

    ch9_v2: add registration and login

--- rango/urls.py
+++ rango/urls.py
@@ -5,9 +5,12 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 ]
+
